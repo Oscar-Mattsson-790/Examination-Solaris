df4d7a18469340e356f93359d1e82f0deedbfd37
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-12-02 08:20:35 -0600

    Complete

--- js/modules/api.js
+++ js/modules/api.js
@@ -2,14 +2,25 @@
 
 // Fetch Data - Här hämtar API och nyckel
 
-const BASE_URL = "https://fathomless-shelf-54969.herokuapp.com";
+const BASE_URL =
+  "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/solaris-api";
 
 async function getKey() {
-  const response = await fetch(`${BASE_URL}/keys`, { method: "POST" });
+  const response = await fetch(`${BASE_URL}/keys`); // Ta bort { method: POST } här
   const data = await response.json();
+  console.log(data);
 
   return data.key;
 }
+
+// const BASE_URL = "https://fathomless-shelf-54969.herokuapp.com";
+
+// async function getKey() {
+//   const response = await fetch(`${BASE_URL}/keys`, { method: "POST" });
+//   const data = await response.json();
+
+//   return data.key;
+// }
 
 async function getPlanets() {
   const key = await getKey();
